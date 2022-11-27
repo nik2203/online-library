bd626ae7ec4eef73f58ec7d7c6d8ae8bce323bb7
nik2203 <62793095+nik2203@users.noreply.github.com>
2022-11-27 22:44:48 +0530

    update

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,8 +6,9 @@ import "./app.css"
 
 function Card(props) {
     return <div>
-        <img src={props.src} height = {props.height}></img>
+        <img src={props.src} height = {100}></img>
         <h2>{props.title}</h2>
+        <h4>By {props.author} </h4>
         <a href={props.link}>Link to archive.org</a>
     </div>
 
---
+++ src/components/card/dump.js
@@ -0,0 +1,32 @@
+const books = [
+    {
+        id: 1,
+        title: "The Romance of Irish History",
+        author: "John Gabriel Rowe",
+        link: "https://archive.org/details/romanceofirishhi00roweuoft"
+    },
+    {
+        id:2,
+        title:"Quaint Irish Bygones",
+        author: "William B Hannon",
+        link: "https://archive.org/details/quaintirishbygon00hanniala"
+    },
+    {
+        id:3,
+        title: "A Woman Alone In The Heart Of Japan",
+        author:"Gertrude Adams Fisher",
+        link: "https://archive.org/details/womanaloneinhear00fishiala"
+    },
+    {
+        id:4,
+        title:"The Picture Of Dorian Gray",
+        author:"Oscar Wilde",
+        link: "https://archive.org/details/pictureofdoriang00wildiala"
+    },
+    {
+        id:5,
+        title:"Demonology and Withcraft",
+        author: "Sir Walter Scott",
+        link: "https://archive.org/details/demonologywitchc00scotiala"
+    }
+]
