a7298cf68eac763cb579e77f8362416fddc29e4f
nik2203 <62793095+nik2203@users.noreply.github.com>
2022-11-27 22:48:06 +0530

    Update card.jsx

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,7 +6,7 @@ import "./app.css"
 
 function Card(props) {
     return <div>
-        <img src={props.src} height = {100}></img>
+        {/*<img src={props.src} height = {100}></img>*/}
         <h2>{props.title}</h2>
         <h4>By {props.author} </h4>
         <a href={props.link}>Link to archive.org</a>
